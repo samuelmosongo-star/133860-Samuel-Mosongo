a36bc0b8495fb0fd5b51b2959a27e748c699e9c4
Samuel Mosongo <samuel.mosongo@strathmore.edu>
2025-10-27 17:37:42 +0300

    initial commit

---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+// This is my first c++ program
+#include <iostream> // Inc lude the input-output stream library
+using namespace std;
+int main()
+{
+    cout << "Hello, World!" << endl; // Output a greeting message
+    cout << "This is my first C++ program" << endl; // Output another message
+
+    return 0; //Indicate that the program ended successfully
+}
