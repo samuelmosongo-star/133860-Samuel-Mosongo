75a3da04c74d6cc5718c54a42f4ab3017a1e7765
Samuel Mosongo <samuel.mosongo@strathmore.edu>
2025-11-03 19:01:55 +0300

    commit

---
+++ EscapeSequence.cpp
@@ -0,0 +1,15 @@
+// Escape sequence operators. Special characters in that
+// control the formatting of output on CLI program.
+#include <iostream> 
+using namespace std;
+int main()
+{
+    cout << "Strathmore University is the leading University in the region \n"; 
+    cout << "\t Testing the tab operator \n";
+    cout << "\\ Testing the backslash operator \n";
+    cout << "\' Testing the single quote operator \n";
+    cout << "\" Testing the double quote operator \n";
+    cout << "\a Testing the alert operator \n";
+    return 0; 
+}
+
