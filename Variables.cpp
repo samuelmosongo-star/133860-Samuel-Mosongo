05a022ee721de0fee8e19bede8362af63dd41427
Samuel Mosongo <samuel.mosongo@strathmore.edu>
2025-11-03 20:29:23 +0300

    commit

---
+++ Variables.cpp
@@ -0,0 +1,34 @@
+// Exploring different variable types in C++
+#include <iostream>
+using namespace std;
+
+// Global Vriables
+int globalVar = 100; /* Variable declared outside any function
+ and accessible from any part of the source file */
+
+int main()
+{
+    int age;
+    int year;
+    int phone;
+
+    // Local Variables
+    int localVar = 50; /* Variable declared within a function
+    and accessible only within that function */
+
+    // Constant Variable
+    const float PI = 3.14159; /* variable whose value cannot be changed
+     after initialization */
+
+    // Static Variable
+    static int staticVar = 0; /* A variable that retains its value
+    between function calls */
+
+    // Displaying the values of different variables
+    cout << "Global Variable: " << globalVar << endl;
+    cout << "Local Variable: " << localVar << endl;
+    cout << "Constant Variable (PI): " << PI << endl;
+    cout << "Static Variable: " << staticVar << endl;
+
+    return 0;
+}
