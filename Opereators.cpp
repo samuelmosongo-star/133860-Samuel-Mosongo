8c4ac9c2df9b4064f4d2071a9d55a0351c582efc
Samuel Mosongo <samuel.mosongo@strathmore.edu>
2025-11-10 20:35:13 +0300

    commit

---
+++ Opereators.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "ADDITION:" << num1 << " + " << num2 << " = " << (num1 + num2) << endl;
+    cout << "SUBTRACTION:" << num1 << " - " << num2 << " = " << (num1 - num2) << endl;
+    cout << "MULTIPLICATION:" << num1 << " * " << num2 << " = " << (num1 * num2) << endl;
+    cout << "DIVISION:" << num1 << " / " << num2 << " = " << (num1 / num2) << endl;
+    cout << "MODULUS:" << num1 << " % " << num2 << " = " << (num1 % num2) << endl;  
+    cout << "INCREMENT:" << "num1++ = " << (num1++) << endl;
+    cout << "DECREMENT:" << "num2-- = " << (num2--) << endl;    
+}
