a45b34fc7ebbaf45333b3b9b6b6663b6e2726f88
Samuel Mosongo <samuel.mosongo@strathmore.edu>
2025-11-17 18:33:47 +0300

    commit

--- operators.cpp
+++ operators.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int main() {
+
+    int num1 =16, num2 = 4;
     cout << "ADDITION:" << num1 << " + " << num2 << " = " << (num1 + num2) << endl;
     cout << "SUBTRACTION:" << num1 << " - " << num2 << " = " << (num1 - num2) << endl;
     cout << "MULTIPLICATION:" << num1 << " * " << num2 << " = " << (num1 * num2) << endl;
@@ -13,5 +15,21 @@ int main() {
     cout << "DECREMENT:" << "num2-- = " << (num2--) << endl;  
     cout << "COMBINED INCREMENT AND ASSIGNMENT: num1 += num2; = " << (num1 += num2) << endl;
     cout << "COMBINED DECREMENT AND ASSIGNMENT: num1 -= num2; = " << (num1 -= num2) << endl;
-    
+    cout << "COMBINED MULTIPLICATION AND ASSIGNMENT: num1 *= num2; = " << (num1 *= num2) << endl;
+    cout << "COMBINED DIVISION AND ASSIGNMENT: num1 /= num2; = " << (num1 /= num2) << endl;
+
+    // COMPARISON/LOGICAL OPERATORS
+    cout << " 3>2 = " << (3>2) << endl;
+    cout << " 3<2 = " << (3<2) << endl; 
+    cout << " 4>=3 = " << (4>=3) << endl;
+    cout << " 4<=3 = " << (4<=3) << endl;
+    cout << " 3==3 = " << (3==3) << endl;
+    cout << " 3!=3 = " << (3!=3) << endl;  
+    cout << " !(4>=3) = " << !(4>=3) << endl;
+
+    // BOOLEAN OPERATORS
+    cout << "  true && true = " << ((3>2) && (4 >= 2)) << endl;
+    cout << " true && false = " << ((3>2) && (4 >= 2)) << endl;
+    cout << " false && true = " << ((3<2) && (4 >= 2)) << endl;
+    cout << " false && false = " << ((3<2) && (4 <= 2)) << endl;    
 }
